2029dd393c91ab9d4635cfb10386670abaf0030a
Joel <142605851+Joel-kaderli@users.noreply.github.com>
2024-03-22 16:23:17 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace dictionaries_übung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> einkaufliste = new Dictionary<string, int>();
+
+            while (true) 
+            {
+                Console.WriteLine("1. Artikel hinzugefügt");
+                Console.WriteLine("2. Einkaufsliste anzeigen");
+                Console.WriteLine("3. Beenden");
+
+                switch (auswahl)
+                {
+                    case 1:
+                        ArtikelHinzufuegen(einkaufsliste);
+                        break;
+                    case 2:
+                        EinkaufslisteAnzeigen(einkaufsliste);
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Ungültige Auswahl. Bitte wählen Sie erneut.");
+                        break;
+                }
+            }
+        }
+    }
+}
